9bc6858ac543e0b7da55750da878f25d373d79e7
Farhad Zamani <zmnfrhd@gmail.com>
2022-10-05 13:36:26 +0200

    edit funcs.py

--- src/funcs.py
+++ src/funcs.py
@@ -7,7 +7,7 @@ def prod(a, b, c):
     Compute the product of three numbers.
 
     >>> prod(1, 2, 3)
-    'TEST ME!'
+    6
     """
     return a * b * c
 
@@ -17,8 +17,8 @@ def prod2(b):
     """
     Get a global a and write to a local c before computing prod(a, b, c)
 
-    >>> prod2(42)
-    'TEST ME'
+    >>> prod2(2)
+    6
     """
 
     c = a
@@ -31,7 +31,7 @@ def longest(x, y):
     Returning the longest of two lists.
 
     >>> longest([1, 2, 3], [4, 5])
-    'TEST ME'
+    [1, 2, 3]
     """
     if len(x) > len(y):
         return x
@@ -45,8 +45,8 @@ def dist(p1, p2):
     """
     Compute the distance between p1 and p2.
 
-    >>> dist((1,2), (3,4))
-    'TEST ME'
+    >>> dist((0,0), (3,4))
+    5
     """
     x1, y1 = p1
     x2, y2 = p2
